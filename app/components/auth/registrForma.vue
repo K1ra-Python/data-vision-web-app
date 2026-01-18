f69929afa8dbc0339941866d75b30a02377ec3c9
K1ra-Python <maksimka.guchenko@mail.ru>
2026-01-18 03:24:37 +0300

    Сделал каркас для странички регистрации

---
+++ app/components/auth/registrForma.vue
@@ -0,0 +1,27 @@
+<
+<template>
+  <div>
+    <form>
+      <input v-model="email" placeholder="EMAIL" required type="email" />email
+      <input
+        v-model="password"
+        placeholder="PASSWORD"
+        required
+        type="password"
+      />pass
+      <button type="submit" @click="submit">send</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const email = ref("");
+const password = ref("")
+const submit = () => {
+  console.log({ email: email.value, password: password.value })
+  // здесь вызов API для регистрации
+}
+</script>
+
+<style></style>
---
+++ app/pages/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+   <registrForma></registrForma>
+  </div>
+</template>
+
+<script setup>
+import registrForma from '~/components/auth/registrForma.vue';
+
+</script>
+
+<style>
+
+</style>
