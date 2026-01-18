3c5d48c9ae1642db168a205ffd15d8ca7b48725f
K1ra-Python <maksimka.guchenko@mail.ru>
2026-01-19 00:24:31 +0300

    #

--- app/components/auth/registrForma.vue
+++ app/components/auth/registrForma.vue
@@ -1,6 +1,15 @@
 <
 <template>
   <div>
+    <div>
+    <h1>Users</h1>
+    <ul v-if="users?.length">
+      <li v-for="user in users" :key="user.id">
+        {{ user.name }} ({{ user.email }})
+      </li>
+    </ul>
+    <p v-else>No users yet.</p>
+  </div>
     <form>
       <input v-model="email" placeholder="EMAIL" required type="email" />email
       <input
@@ -18,9 +27,11 @@
 import { ref } from "vue";
 const email = ref("");
 const password = ref("")
+const { data: users } = await useFetch('/api/users')
 const submit = () => {
   console.log({ email: email.value, password: password.value })
   // здесь вызов API для регистрации
+  
 }
 </script>
 
---
+++ prisma/migrations/20260118211409_im_tired/migration.sql
@@ -0,0 +1,50 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password" TEXT NOT NULL,
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- CreateTable
+CREATE TABLE "Dataset" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "dataUrl" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Dataset_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Template" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "config" JSONB NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Template_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Dataset" ADD CONSTRAINT "Dataset_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Template" ADD CONSTRAINT "Template_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
