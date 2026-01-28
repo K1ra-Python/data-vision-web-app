19169352680c30421bdcf36747c54e3648aee7ee
K1ra-Python <maksimka.guchenko@mail.ru>
2026-01-28 10:50:43 +0300

    Добавлена Форма логина и страница логина, прописан апи для логина

---
+++ app/pages/auth.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <loginForma></loginForma>
+  </div>
+</template>
+
+<script setup>
+import loginForma from '~/components/auth/loginForma.vue';
+</script>
+
+<style>
+
+</style>
